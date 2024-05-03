648eaffae0579ecdf88cd8285addcd634dd4b443
gvaa <golobokin@gmail.com>
2024-05-03 23:45:52 +0200

    Create initial project structure. Add base functions

---
+++ scripts/kalku.js
@@ -0,0 +1,30 @@
+"use strict";
+
+// https://en.wikipedia.org/wiki/Kalku
+
+let operator;
+let operandOne;
+let operandTwo;
+
+let add = function (a, b) {
+    return a + b;
+}
+
+let subtract = function (a, b) {
+    return a - b;
+}
+
+let multiply = function (a, b) {
+    return a * b;
+}
+
+let divide = function (a, b) {
+    return a / b;
+}
+
+let operate = function (operator, operandOne, operandTwo)
+
+console.log(add(5,9));
+console.log(subtract(5,9));
+console.log(multiply(5,9));
+console.log(divide(5,9));
